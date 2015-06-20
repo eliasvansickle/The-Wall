b4d8cafc177b92ffa54e2c691b35828fccaec3a8
eliasvansickle <eliasvansickle@gmail.com>
2015-06-20 16:11:46 -0700

    wall done

--- main.php
+++ main.php
@@ -5,9 +5,11 @@
 	$query_message_data = "SELECT * FROM users JOIN messages ON users.id = messages.user_id ORDER BY messages.created_at DESC";
 	$_SESSION['message_feed'] = fetch($query_message_data);
 
-	$query_comment_data = "SELECT * FROM messages JOIN comments ON messages.id = comments.message_id";
-
+	$query_comment_data = "SELECT * FROM messages JOIN comments ON messages.id = comments.message_id JOIN users ON messages.user_id = users.id";
 	$_SESSION['comment_feed'] = fetch($query_comment_data);
+
+	// $query_comment_person = "SELECT * FROM comments JOIN messages ON comments.message_id = messages.id JOIN users ON messages.user_id = users.id";
+	// $_SESSION['comment_person'] = fetch($query_comment_person);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -45,17 +47,14 @@
 			<?php
 			if(isset($_SESSION['message_feed']) && !empty($_SESSION['message_feed'])) {
 				foreach ($_SESSION['message_feed'] as $message) {
-					echo "<div class='message_feed'><p>".$message['first_name']." ".$message['last_name']."- ".date('F jS Y', strtotime($message['created_at']))."</p><br>".$message['message']."</div>";	
-					// var_dump($message);
-					// die();		
+					echo "<div class='message_feed'><p>".$message['first_name']." ".$message['last_name']."- ".date('F jS Y', strtotime($message['created_at']))."</p><br>".$message['message']."</div>";			
 			 ?>
 			<div id='comment'>
 				<?php if(isset($_SESSION['comment_feed']))
 				{
 					foreach ($_SESSION['comment_feed'] as $comment) {
 						if($comment['message_id'] == $message['id']) {
-
-						echo "<span class='comments'><p>".$comment['comment']. "</p></span><br>";
+						echo $comment['first_name']." ".$comment['last_name']."- ".date('F jS Y', strtotime($comment['created_at']))."<br><span class='comments'><p>".$comment['comment']. "</p></span><br>";
 						}
 					}
 				} ?>
